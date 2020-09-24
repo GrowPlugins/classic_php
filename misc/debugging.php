e9e6b7517a09da85191603472f8e833554dba184
DynAggelos <35944457+DynAggelos@users.noreply.github.com>
2020-09-23 22:38:30 -0500

    finish first draft of unit_test()

--- misc/debugging.php
+++ misc/debugging.php
@@ -42,5 +42,85 @@ namespace ClassicPHP {
             /* Return ****************************************************/
             return $time2 - $time1;
         }
+
+        /** @method unit_test
+         * Executes a callback function and returns the time it took to
+         * execute it, for comparison with other benchmarked callback
+         * functions.
+         * @param callable $callback_function
+         * @param array $callback_function_parameters
+         * @return float
+         */
+        function unit_test(
+            callable $callback_function,
+            $expected_result,
+            array $callback_function_parameters = [] ) {
+
+            /* Declaration ***********************************************/
+            $result;
+            $result_message;
+            $result_var_dump;
+            $expected_result_var_dump;
+
+            /* Processing ************************************************/
+            /* Execute Function/Method */
+            try {
+
+                $result = call_user_func_array(
+                    $callback_function,
+                    $callback_function_parameters);
+            }
+            catch (\Error $e) {
+
+                return
+                    'Error Thrown: ' . $e->getCode() . '. '
+                    . $e->getMessage() . '. Thrown in ' . $e->getFile()
+                    . ', line ' . $e->getLine();
+            }
+            catch (\Exception $e) {
+
+                return
+                    'Error Thrown: ' . $e->getCode() . '. '
+                    . $e->getMessage() . '. Thrown in ' . $e->getFile()
+                    . ', line ' . $e->getLine();
+            }
+
+            /* Test Result of Execution ---------------------------------*/
+            /* Prepare Test Result Message Based on Result Found */
+            if ( $result === $expected_result ) {
+
+                $result_message = 'Test Passed! ';
+            }
+            elseif ( $result == $expected_result ) {
+
+                $result_message = 'Partially Passed. ';
+            }
+            else {
+                $result_message = 'Test Failed. ';
+            }
+
+            /* Gather Result Var Dump Info */
+            // Get Var Dump of $expected_result Variable
+            ob_start();
+
+            var_dump( $expected_result );
+
+            $expected_result_var_dump = ob_get_contents();
+
+            // Get Var Dump of $result Variable
+            ob_clean();
+
+            var_dump( $result );
+
+            $result_var_dump = ob_get_clean();
+
+            /* Append Result Info */
+            $result_message .=
+                "\nExpected Result: { " . $expected_result_var_dump
+                . "}\n Result: { " . $result_var_dump . '}';
+
+            /* Return ****************************************************/
+            return $result_message;
+        }
     }
 }
