0c21e35197273365cf4abe4610f74924a5450197
DynAggelos <35944457+DynAggelos@users.noreply.github.com>
2020-09-23 21:51:22 -0500

    update throw_error() to simplify it's purpose

--- misc/error_handling.php
+++ misc/error_handling.php
@@ -3,7 +3,7 @@
 namespace ClassicPHP {
 
     /** Class: ErrorHandling
-     * Allows you to manage the handling of error easier.
+     * Helps you to manage the handling of errors easier.
      * Inherits From: None
      * Requires: None
      * Inherited By: None
@@ -15,27 +15,21 @@ namespace ClassicPHP {
         ******************************************************************/
 
         /** @method throw_error
-         * Allows you to throw an error or echo one. The error can include
-         * information about variables related to the error. If error
-         * handling is set to echo the error to the screen, echoing can be
-         * put within a <pre> element for nicer display.
+         * Allows you to throw an error. Can optionally use "pretty" error
+         * message formatting, which uses additional line breaks and puts
+         * any screen output within a <pre> element for nicer display.
          * @param string $error_description
          * @param string $error_level
-         * @param mixed[] $variables
-         * @param bool $echo
-         * @param bool $output_pre_wrapper
+         * @param bool $pretty_output
          */
         function throw_error(
             string $error_description,
             string $error_level = 'warning',
-            $variables = [],
-            bool $echo = false,
-            bool $output_pre_wrapper = false ) {
+            bool $pretty_output = false ) {
 
             /* Declaration ***********************************************/
             $backtrace =
                 debug_backtrace( DEBUG_BACKTRACE_PROVIDE_OBJECT, 20 );
-            $vardump = '';
             $error_message = '';
             $error_type;
 
@@ -55,97 +49,26 @@ namespace ClassicPHP {
                 $error_type = E_USER_ERROR;
             }
 
-            /* Gather Information About Input Variable(s) ---------------*/
-            /* Generate $vardump String If $variables Not Null Array */
-            if ( [] !== $variables ) {
-
-                ob_start();
-
-                var_dump($variables);
-
-                $vardump = ob_get_clean();
-            }
-
             /* Build Error Message --------------------------------------*/
-            /* Append Description */
-            $error_message .= $error_description . "\n\n";
+            $error_message = $error_description . "\nBacktrace:\n";
 
-            /* Append Backtrace Data */
-            $error_message .= "Backtrace:\n";
-
-            for( $i = 0; $i < count( $backtrace ); $i++ ) {
-
-                // Output Backtrace Index
-                $error_message .= ( $i + 1) . '. ';
-
-                // Output Class and Function in Backtrace
-                if ( isset( $backtrace[ $i ]['class'] ) ) {
-
-                    $error_message .= $backtrace[ $i ]['class'] . '::';
-                }
-
-                $error_message .=
-                    $backtrace[ $i ]['function'] . '()';
-
-                // Output Location Called From
-                if ( 0 < $i ) {
-
-                    $error_message .=
-                        ' at ' . $backtrace[ $i - 1 ]['file']
-                        . ', line ' . $backtrace[ $i - 1 ]['line'];
-                }
-
-                // Conditionally Output Last of Called From Sentence
-                if ( $i + 1 < count( $backtrace ) ) {
-
-                    if ( 0 < $i ) {
-
-                        $error_message .= ', which';
-                    }
-
-                    $error_message .= " was called by\n";
-                }
-                else {
-
-                    $error_message .=
-                        ", which originated from\n" . ( $i + 2 ) . '. '
-                        . $backtrace[ $i ]['file'] . ', line '
-                        . $backtrace[ $i ]['line'] . "\n";
-                }
-            }
-
-            /* Append Var Dump Data */
-            if ( '' !== $vardump ) {
-
-                $error_message .= "\nVariable Dump:\n" . $vardump;
-            }
+            $error_message .= $this->format_backtrace(
+                $backtrace,
+                $pretty_output );
 
             /* Output Error Information ---------------------------------*/
-            if ( false === $echo ) {
+            if ( ! $pretty_output ) {
 
-                if ( false === $output_pre_wrapper ) {
-
-                    trigger_error(
-                        htmlentities( $error_message ),
-                        $error_type );
-                }
-                else {
-                    echo '<pre>';
-                    trigger_error(
-                        htmlentities( $error_message ),
-                        $error_type );
-                    echo '</pre>';
-                }
+                trigger_error(
+                    htmlentities( $error_message ),
+                    $error_type );
             }
             else {
-
-                if ( false === $output_pre_wrapper ) {
-
-                    echo $error_message;
-                }
-                else {
-                    echo '<pre>' . $error_message . '</pre>';
-                }
+                echo '<pre>';
+                trigger_error(
+                    htmlentities( $error_message ),
+                    $error_type );
+                echo '</pre>';
             }
         }
 
@@ -212,7 +135,7 @@ namespace ClassicPHP {
                 $error_message .= "\nVariable Dump:\n" . $vardump;
             }
 
-            /* Output Error Information ---------------------------------*/
+            /* Throw Error with Error Message ---------------------------*/
             if ( ! $pretty_output ) {
 
                 trigger_error(
@@ -287,6 +210,10 @@ namespace ClassicPHP {
                 echo '</pre>';
             }
         }
+
+        /******************************************************************
+        * Private Methods
+        ******************************************************************/
 
         /** @method format_backtrace
          * Returns a prettified backtrace.
