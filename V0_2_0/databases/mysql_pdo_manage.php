7f87e405f331751847b0090df5b8e84c63c22d92
DynAggelos <35944457+DynAggelos@users.noreply.github.com>
2021-12-09 11:53:29 -0600

    move to new semantic versioning, starting with version 0.2.0

--- V0_2_0/databases/mysql_pdo_manage.php
+++ V0_2_0/databases/mysql_pdo_manage.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace ClassicPHP\V0_1_2;
+namespace ClassicPHP\V0_2_0;
 
 /**************************************************************************
  * Class Header -----------------------------------------------------------
@@ -12,7 +12,7 @@ use \PDO as PDO;
 // Determine ClassicPHP Base Path
 if ( ! defined( 'CLASSIC_PHP_DIR' ) ) {
 
-    $dir = strstr( __DIR__, 'classic_php', true ) . 'classic_php/V0_1_2';
+    $dir = strstr( __DIR__, 'classic_php', true ) . 'classic_php/V0_2_0';
 
     define( 'CLASSIC_PHP_DIR', $dir );
 
